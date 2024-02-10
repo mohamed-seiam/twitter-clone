60d10096836a391498000cb5d2f5d03ccdba1ec1
mohamedSeiam <midoredase@gmail.com>
2024-02-10 21:22:06 +0200

    -done with login -done with signUp -added palletCLass -added AssetsCLass

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:twitter_clone/features/auth/view/signup_view.dart';
+import 'package:twitter_clone/theme/theme.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.theme,
+        home: const SignUpView(),
+      ),
+    );
+  }
+}
